2d8799017ca4416966df7f84be2f4ebcdc5da8cc
Heisnberg <Matrix1101@protonmail.com>
2024-03-08 13:45:33 +0800

    TopAppBar has been implemented

--- app/src/main/java/com/example/wea_cal_note/MainActivity.kt
+++ app/src/main/java/com/example/wea_cal_note/MainActivity.kt
@@ -3,22 +3,44 @@ package com.example.wea_cal_note
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import com.example.wea_cal_note.calendar.CalendarUI
+import com.example.wea_cal_note.ui.theme.Blue10
 import com.example.wea_cal_note.ui.theme.WeaCalNoteTheme
+import com.example.wea_cal_note.ui.theme.fontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (!Python.isStarted()) {
-            Python.start( AndroidPlatform(this))
+            Python.start(AndroidPlatform(this))
         }
         setContent {
             WeaCalNoteTheme {
@@ -34,9 +56,65 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    CalendarUI()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "11°" + "  " + "阴" + " ，绵阳",
+                        fontFamily = fontFamily,
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.LocationOn,
+                            contentDescription = "Location for weather"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.List, contentDescription = "Events")
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Settings, contentDescription = "Settings"
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "MoreInfo")
+                    }
+                },
+            )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "Add Events")
+            }
+        },
+
+        ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        ) {
+            CalendarUI()
+        }
+    }
 }
 
 @Preview
